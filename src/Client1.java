4320a06817d9213d1491eb06a7a25ed2e663d016
ATrestRV <jasoendn03@gmail.com>
2022-07-08 16:05:30 +0700

    Multithreading Chatbox (GUI)

--- src/Client1.java
+++ src/Client1.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
 
@@ -5,25 +9,44 @@ public class Client1 {
     BufferedReader keyboard;
     BufferedReader getMessage;
     PrintWriter writer;
+    JTextField outcoming;
+    JTextArea incoming;
+
     public static void main(String[] args) {
         Client1 c = new Client1();
+        c.buildGUI();
         c.connectToTheServer();
+    }
+
+    public void buildGUI() {
+        JFrame frame = new JFrame("Client");
+        JPanel northPanel = new JPanel();
+        JPanel southPanel = new JPanel();
+        JButton sendButton = new JButton("Send");
+        outcoming = new JTextField(50);
+        incoming = new JTextArea(20,50);
+        northPanel.add(incoming);
+        southPanel.add(outcoming);
+        southPanel.add(sendButton);
+        sendButton.addActionListener(new SendListener());
+        JScrollPane scrollPane = new JScrollPane(incoming);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        northPanel.add(scrollPane);
+        frame.getContentPane().add(BorderLayout.SOUTH,southPanel);
+        frame.getContentPane().add(northPanel);
+        frame.setVisible(true);
+        frame.pack();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public void connectToTheServer() {
         try {
             Socket socket = new Socket("localhost",2014);
-            keyboard = new BufferedReader(new InputStreamReader(System.in));
             getMessage = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
-            String message = "";
             Thread t = new Thread(new ReceiveMessage());
             t.start();
-            while (!message.equalsIgnoreCase("quit")) {
-                System.out.println(">");
-                message = keyboard.readLine();
-                writer.println(message);
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -32,15 +55,25 @@ public class Client1 {
     public class ReceiveMessage implements Runnable {
         @Override
         public void run() {
-            while (true) {
+            String inMessage = "";
+            while (!inMessage.equalsIgnoreCase("quit")) {
                 try {
-                    System.out.println("Server: "+getMessage.readLine());
+                    inMessage = getMessage.readLine();
+                    incoming.append("Server: "+inMessage+"\n");
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
             }
+        }
+    }
 
-
+    private class SendListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String outMessage = outcoming.getText();
+            outcoming.setText("");
+            writer.println(outMessage);
+            incoming.append("\t\t\t\tYou: "+outMessage+"\n");
         }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,43 +13,72 @@ public class Server {
     BufferedReader getMessage;
     BufferedReader keyboard;
     PrintWriter writer;
+    JFrame frame;
+    JTextArea incoming;
+    JTextField outcoming;
     public static void main(String[] args) {
         Server s = new Server();
-        s.acceptClient();
+        s.buildGUI();
+        s.startServer();
     }
 
-    public void acceptClient() {
+    public void buildGUI() {
+        frame = new JFrame("Server");
+        JPanel northPanel = new JPanel();
+        JPanel southPanel = new JPanel();
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new SendListener());
+        incoming = new JTextArea(20,50);
+        outcoming = new JTextField(50);
+        northPanel.add(incoming);
+        southPanel.add(outcoming);
+        southPanel.add(sendButton);
+        JScrollPane scrollPane = new JScrollPane(incoming);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        northPanel.add(scrollPane);
+        frame.getContentPane().add(BorderLayout.SOUTH,southPanel);
+        frame.getContentPane().add(northPanel);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void startServer() {
         try {
             ServerSocket serverSocket = new ServerSocket(2014);
             Socket clientSocket = serverSocket.accept();
             getMessage = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            keyboard = new BufferedReader(new InputStreamReader(System.in));
             writer = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()),true);
-            Thread t = new Thread(new sendMessage());
+            Thread t = new Thread(new ReceiveMessage());
             t.start();
-            while (true) {
-                System.out.println("Client: "+getMessage.readLine());
-            }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
     }
+    private class SendListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String outMessage = outcoming.getText();
+            writer.println(outMessage);
+            outcoming.setText("");
+            incoming.append("\t \t \t \t You: "+outMessage+"\n");
+        }
+    }
 
-    class sendMessage implements Runnable{
+    class ReceiveMessage implements Runnable {
         @Override
         public void run() {
             try {
-                while (true) {
-                    System.out.println("> ");
-                    String message = keyboard.readLine();
-                    writer.println(message);
+                String inMessage = "";
+                while (!inMessage.equalsIgnoreCase("quit")) {
+                    inMessage = getMessage.readLine();
+                    incoming.append("Client: "+inMessage+"\n");
                 }
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
-
         }
     }
-
 }
